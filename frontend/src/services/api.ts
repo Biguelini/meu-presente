03a718894bc0094e6e6d58fba356fbbd73457a48
Biguelini <biguelinijoao@gmail.com>
2025-12-08 22:19:40 -0300

    feat: preparing for deploy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { ApiResponse } from '../types';
 
-const API_URL = '/api';
+const API_URL = import.meta.env.VITE_API_URL || '/api';
 
 const api = axios.create({
   baseURL: API_URL,
